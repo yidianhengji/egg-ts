8b6d881de6458635c0a5ffc176598e34fdb2caa4
yidianhengji <493871850@qq.com>
2020-02-26 14:46:48 +0800

    登录注释

--- app/service/SysAdmin.ts
+++ app/service/SysAdmin.ts
@@ -4,18 +4,22 @@ import md5 = require('md5');
 import { CONSTANT } from '../utils/constant';
 
 export default class SysAdmin extends Service {
-  public async login(params: any) {
+  public async login(params: { phone: string; password: string; }) {
     const ctx = this.ctx;
+    // 校验账号没有传抛出错误异常
     if (!params.phone) {
       ctx.throw(CONSTANT.FAILCODE, CONSTANT.ACCOUNTNUMBERCANNOTBEEMPTY)
     }
+    // 校验密码没有传抛出错误异常
     if (!params.password) {
       ctx.throw(CONSTANT.FAILCODE, CONSTANT.PASSWORDCANNOTBEEMPTY)
     }
     const nonExistent = await ctx.model.SysAdmin.findAll({ where: { phone: params.phone } });
     const data = await ctx.model.SysAdmin.findAll({ where: { phone: params.phone, password: md5(params.password) } });
+    // 校验账号是否存在
     if (nonExistent.length <= 0) {
       ctx.throw(CONSTANT.FAILCODE, CONSTANT.ACCOUNTDOESNOTEXIST)
+      // 校验密码是否正确
     } else if (data.length <= 0) {
       ctx.throw(CONSTANT.FAILCODE, CONSTANT.PASSWORDERROR)
     } else {
