f1dd7be7ac2de0edf8b6782d8cc81cf13ccf398b
yidianhengji <493871850@qq.com>
2020-02-26 12:27:00 +0800

    优化登录

--- app/service/SysAdmin.ts
+++ app/service/SysAdmin.ts
@@ -1,22 +1,32 @@
 import { Service } from 'egg';
 import jwt = require('jsonwebtoken');
 import md5 = require('md5');
-
+import { CONSTANT } from '../utils/constant';
 
 export default class SysAdmin extends Service {
   public async login(params: any) {
     const ctx = this.ctx;
-    const query = {
-      where: {
-        phone: params.phone,
-        password: md5(params.password)
-      }
-    };
-    const data = await ctx.model.SysAdmin.findAll(query);
-    if (data) {
-      const token = jwt.sign(JSON.stringify(data), ctx.app.config.jwt.secret, { expiresIn: ctx.app.config.jwt.expiresIn })
+    if (!params.phone) {
+      ctx.throw(CONSTANT.FAILCODE, CONSTANT.ACCOUNTNUMBERCANNOTBEEMPTY)
+    }
+    if (!params.password) {
+      ctx.throw(CONSTANT.FAILCODE, CONSTANT.PASSWORDCANNOTBEEMPTY)
+    }
+    const nonExistent = await ctx.model.SysAdmin.findAll({ where: { phone: params.phone } });
+    const data = await ctx.model.SysAdmin.findAll({ where: { phone: params.phone, password: md5(params.password) } });
+    if (nonExistent.length <= 0) {
+      ctx.throw(CONSTANT.FAILCODE, CONSTANT.ACCOUNTDOESNOTEXIST)
+    } else if (data.length <= 0) {
+      ctx.throw(CONSTANT.FAILCODE, CONSTANT.PASSWORDERROR)
+    } else {
+      const token = jwt.sign(JSON.stringify(data), ctx.app.config.jwt.secret)
       return {
-        token
+        message: CONSTANT.LOGINMSG,
+        status: CONSTANT.OKCODE,
+        data: {
+          token,
+          data
+        }
       }
     }
   }
